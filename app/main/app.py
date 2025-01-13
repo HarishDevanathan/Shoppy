152772090913a25cc73c2be72883d960a547e312
Ganesh Kumar <ganeshkumar@Ganeshs-MacBook-Air.local>
2025-01-13 22:45:57 +0530

    password

--- app/main/app.py
+++ app/main/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder='../templates')
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Harish123@localhost/project_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:ganesh2005*@localhost/project_db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
