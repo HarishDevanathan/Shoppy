1b45d0694b6ac203a271d66d1bf0fed16fdede5b
Ganesh Kumar <ganeshkumar@Ganeshs-MacBook-Air.local>
2025-01-13 12:47:29 +0530

    nav bar tested

--- app/main/app.py
+++ app/main/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__,
 
 @app.route('/')
 def base():
-    return render_template("base.html")
+    return render_template("aboutuspage.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
