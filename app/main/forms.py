77c149429529eeb4c03fd269588a30e72a9a57df
HarishDevanathan <harishdevanathan123@gmail.com>
2025-01-15 13:46:41 +0530

    changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,6 +7,31 @@ from wtforms.validators import DataRequired,Length
 class LoginForm(FlaskForm):
     username=StringField('username',validators=[DataRequired()])
     password=PasswordField('password',validators=[DataRequired(),Length(min=6,max=16)])
+    
 
+class SignForm(FlaskForm):
+    username = StringField('username', validators=[Required(), Length(1, 100), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,'Usernames must have only letters, ''numbers, dots or underscores')])
+    password = PasswordField('uassword', validators=[
+        DataRequired(),
+        Length((6,12), message='Password must be between 6 to 12 characters long'),
+        Regexp(r'[A-Z]', message='Password must contain at least one uppercase letter'),
+        Regexp(r'[a-z]', message='Password must contain at least one lowercase letter'),
+        Regexp(r'[0-9]', message='Password must contain at least one digit'),
+        Regexp(r'[@$!%*?&]', message='Password must contain at least one special character (@, $, !, %, *, ?, &)'),
+    ])
+    email = StringField('email', validators=[
+        DataRequired(),
+        Email(message='Invalid email address')
+    ])
+    phno= StringField('phno',validators=[
+        DataRequired(),
+        Length(10)
+    ])
+    address= StringField('address',validators=[
+        DataRequired(),
+        Length(5,100)
+    ])
+    
 
+    
 
