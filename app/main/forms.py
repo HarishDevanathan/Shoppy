d969ca789240aabc7bf411d0d01ebfd733c8df6d
HarishDevanathan <harishdevanathan123@gmail.com>
2025-01-15 19:17:28 +0530

    changes

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,14 +9,9 @@ class LoginForm(FlaskForm):
     password=PasswordField('password',validators=[DataRequired(),Length(min=6,max=16)])
     
 
-<<<<<<< HEAD
-class SignupForm(FlaskForm):
-    username = StringField('username', validators=[Required(), Length(1, 100), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,'Usernames must have only letters, ''numbers, dots or underscores')])
-=======
 class SignForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), Length(1, 100), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,'Usernames must have only letters, ''numbers, dots or underscores')])
->>>>>>> 8f4078fdd144be92867c9c1465fc7326680ad872
-    password = PasswordField('uassword', validators=[
+    password = PasswordField('password', validators=[
         DataRequired(),
         Length((8,16), message='Password must be between 8 to 16 characters long'),
         Regexp(r'[A-Z]', message='Password must contain at least one uppercase letter'),
