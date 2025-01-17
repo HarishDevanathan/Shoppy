964f81d6c34754e2e1e6d89cfa95597714639ec7
Ganesh Kumar <ganeshkumar@Ganeshs-MacBook-Air.local>
2025-01-17 12:40:10 +0530

    product table added

--- db/Schema.sql
+++ db/Schema.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 9.1.0, for macos13.7 (arm64)
 --
 -- Host: localhost    Database: project_db
 -- ------------------------------------------------------
@@ -14,6 +14,39 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `name` varchar(100) DEFAULT NULL,
+  `brand` varchar(100) DEFAULT NULL,
+  `product_id` varchar(16) DEFAULT NULL,
+  `id` varchar(11) DEFAULT NULL,
+  `discountpercentage` int DEFAULT NULL,
+  `discountprice` int DEFAULT NULL,
+  `imagepath` varchar(200) DEFAULT NULL,
+  `productdescp` varchar(200) DEFAULT NULL,
+  `rating` int DEFAULT NULL,
+  `stock` int DEFAULT NULL,
+  `units_sold` int DEFAULT NULL,
+  KEY `id` (`id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user_data` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_data`
@@ -61,4 +94,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-16 19:41:11
+-- Dump completed on 2025-01-17 12:39:56
